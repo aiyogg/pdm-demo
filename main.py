9118efed4b4ce02c268166f40ee004560a02998c
chuck <tengchenc@gmail.com>
2022-06-05 21:37:46 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from json.tool import main
+from requests import request
+from pydash import _
+
+# get GitHub user followers
+
+
+def get_followers(user):
+    url = 'https://api.github.com/users/' + user + '/followers'
+    return request('GET', url).json()
+
+
+if __name__ == "__main__":
+    result = get_followers('aiyogg')
+    print('result: ', _.map_(result, 'login'))
